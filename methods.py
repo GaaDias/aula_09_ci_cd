5ab1c72e887f0d47e80d56fd82a6cd57513f41f8
Gabriel Dias <diasgabriel246@gmail.com>
2023-05-03 09:21:14 -0300

    Ex. 1 - aula 9 - 4 funções calculadora
    Signed-off-by: Gabriel Dias <diasgabriel246@gmail.com>

--- methods.py
+++ methods.py
@@ -5,3 +5,15 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def test_soma(a,b):
+    return a + b
+
+def test_subtracao(a,b):
+    return a - b
+
+def test_multiplicacao(a,b):
+    return a * b
+
+def test_divisao(a,b):
+    return a / b
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    # given an a of 32 and a b of 54
+    a = 32
+    b = 54
+
+    # when we calculate the sum
+    output = methods.test_soma(a,b)
+    
+    # then the sum should be 86
+    assert output == 86
+
+def test_subtracao():
+    # given an a of 32 and a b of 16
+    a = 32
+    b = 16
+
+    # when we calculate the subtraction
+    output = methods.test_subtracao(a, b)
+    
+    # then the subtraction should be 16
+    assert output == 16
+
+def test_multiplicacao():
+    # given an a of 4 and a b of 11
+    a = 4
+    b = 11
+
+    # when we calculate the multiplication
+    output = methods.test_multiplicacao(a, b)
+    
+    # then the multiplication should be 44
+    assert output == 44
+
+def test_divisao():
+    # given an a of 32 and a b of 16
+    a = 32
+    b = 16
+
+    # when we calculate the division
+    output = methods.test_divisao(a, b)
+    
+    # then the division should be 2
+    assert output == 2
